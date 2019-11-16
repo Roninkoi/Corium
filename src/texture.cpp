19954ecc932a30e04384fb99455ad5435ab4d807
rak <roninkoi@iki.fi>
2019-11-17 01:43:54 +0200

    Ignore CMake

--- src/texture.cpp
+++ src/texture.cpp
@@ -5,7 +5,7 @@
 #include <texture.h>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "util/stb_image.h"
+#include "../lib/include/stb_image.h"
 
 unsigned char *loadGLTex(std::string p, int *w, int *h)
 {
